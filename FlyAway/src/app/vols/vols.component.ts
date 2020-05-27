8919271787e840ce59d99200238db60656939eb8
Csab1 <55100500+Csab1@users.noreply.github.com>
2020-05-27 14:21:47 +0200

    add route

---
+++ FlyAway/src/app/vols/vols.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vols',
+  templateUrl: './vols.component.html',
+  styleUrls: ['./vols.component.scss'],
+})
+export class VolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
