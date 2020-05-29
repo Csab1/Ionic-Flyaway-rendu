9acc72c2bf0c0f7ad46ba6ab975f6dfeebcc1d0d
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-05-29 15:50:53 +0200

    Added Billet component

---
+++ src/app/research/research.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-research',
+  templateUrl: './research.component.html',
+  styleUrls: ['./research.component.scss'],
+})
+export class ResearchComponent implements OnInit {
+
+  constructor(public router: Router, public afAuth: AngularFireAuth) { }
+
+  ngOnInit() {}
+
+}
