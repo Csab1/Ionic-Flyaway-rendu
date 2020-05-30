1ff5657c20b532457cb359310f156779101bbd7e
antoine75 <Corsica1945!>
2020-05-31 00:08:28 +0200

    mini update menu

--- FlyAway/src/app/param/param.component.spec.ts
+++ FlyAway/src/app/param/param.component.spec.ts
@@ -2,6 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ParamComponent } from './param.component';
+import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from '../../component/popover/popover.component';
 
 describe('ParamComponent', () => {
   let component: ParamComponent;
@@ -9,8 +12,8 @@ describe('ParamComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ParamComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ParamComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ParamComponent);
