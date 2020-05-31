b9de7263f635e48e3843087b733bde34a65ebb57
antoine75 <Corsica1945!>
2020-05-31 17:40:13 +0200

    update

---
+++ FlyAway/src/app/addcb/addcb.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addcb',
+  templateUrl: './addcb.page.html',
+  styleUrls: ['./addcb.page.scss'],
+})
+export class AddcbPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
